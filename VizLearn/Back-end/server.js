64ab941462b98a6c4826073968d7e160209b6471
KhuranaNiharika <khuranan@uwindsor.ca>
2023-07-14 04:59:24 -0400

    feat(login): added api for signup and signin

--- VizLearn/Back-end/server.js
+++ VizLearn/Back-end/server.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-// const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const { default: mongoose } = require('mongoose');
-const MongoClient = require('mongodb').MongoClient;
 
 const app = express();
 app.use(bodyParser.json());
@@ -21,9 +20,50 @@ mongoose
     console.error('Failed to connect to the database:', error);
   });
 
+app.post('/signup', async (req, res) => {
+    try {
+        const { username, password,email,firstName,lastName } = req.body;
 
-app.get('/', (req, res) => {
-    res.send("Hello Niharika");
+        const existingUser = await db.collection('users').findOne({ username });
+        if (existingUser) {
+            return res.status(400).send({ error: 'Username already exists' });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = { username, password: hashedPassword, email, firstName, lastName };
+        const result = await db.collection('users').insertOne(user);
+
+        const token = jwt.sign({ id: result.insertedId }, 'your_jwt_secret');
+
+        res.status(201).send({ token });
+    } catch (err) {
+        res.status(500).send({ error: 'An error has occurred. Please try again.' });
+    }
+});
+
+app.post('/signin', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+
+        // Check if user exists
+        const user = await db.collection('users').findOne({ username });
+        if (!user) {
+            return res.status(400).send({ error: 'User does not exist' });
+        }
+
+        // Check if password is correct
+        const isValidPassword = await bcrypt.compare(password, user.password);
+        if (!isValidPassword) {
+            return res.status(400).send({ error: 'Invalid password' });
+        }
+
+        const token = jwt.sign({ id: user._id }, 'your_jwt_secret');
+
+        res.status(200).send({ token });
+    } catch (err) {
+        res.status(500).send({ error: 'An error has occurred. Please try again.' });
+    }
 });
 
 app.listen(3000, () => console.log('Server running on port 3000'));
